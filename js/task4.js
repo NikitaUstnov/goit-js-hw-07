005b38aaa1f5e4f27fbb5a456c30fbeeca4bd5ff
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-10-02 17:37:34 +0300

    ТАЫЛ4

---
+++ js/task4.js
@@ -0,0 +1,15 @@
+const onMinus = document.querySelector('[data-action="decrement"]');
+const onPlus = document.querySelector('[data-action="increment"]');
+const counValue = document.getElementById("value");
+
+// let cunt = 0;
+// // counValue.addEventListener("click", onClick);
+
+// const onClick = function () {
+//   console.log("gh");
+//   //   cunt = cunt + 1;
+//   //   return cunt;
+// };
+
+// counValue.addEventListener("click", onClick);
+// console.log(counValue.textContent);
